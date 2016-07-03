094f9f9fdab954745adc94fbdcba0ed91d98b5c5
kuantumlad <brandon.clary@uconn.edu>
2016-07-02 23:53:00 -0400

    3

--- testphp/index.php
+++ testphp/index.php
@@ -6,7 +6,7 @@
 <h1>Welcome to my home page!</h1>
 <p>Some text.</p>
 <p>Some more text.</p>
-<?php include '/includes/header.php';?>
+<?php include '/userweb/bclary/public_html/WebDev/testphp/includes/header.php';?>
 
 </body>
 </html>
