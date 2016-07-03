79b0745e529093ccd4993bd39544dbd4dc87cf7f
kuantumlad <brandon.clary@uconn.edu>
2016-07-03 01:34:45 -0400

    8

--- testphp/index.php
+++ testphp/index.php
@@ -11,7 +11,7 @@
 </head>
 
 
-<?php include 'includes/footer.php';?>
+<?php include 'includes/header.php';?>
  
 
  
@@ -50,7 +50,7 @@
       <li>Advisor: Kyungseon Joo - email: kjoo@phys.uconn.edu</li>
       <li>Graduate Student: Brandon A. Clary - email:brandon.clary@uconn.edu</li>
       <li>Copyright © Brandon Clary</li>
-      <li><!-- hhmts start -->Last modified: Sun Jul  3 01:23:22 EDT 2016 <!-- hhmts end --></li>
+      <li><!-- hhmts start -->Last modified: Sun Jul  3 01:34:27 EDT 2016 <!-- hhmts end --></li>
     </ol>
 </div>
 
