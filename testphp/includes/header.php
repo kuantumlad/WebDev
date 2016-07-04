77dc03b345b8317f16aa0a1ef21a21c5a1428a3f
kuantumlad <brandon.clary@uconn.edu>
2016-07-03 21:02:57 -0400

    change homepage ref

--- testphp/includes/header.php
+++ testphp/includes/header.php
@@ -1,6 +1,6 @@
 <?php
 echo '<div id="navigationContainer">    
-<a href="https://userweb.jlab.org/~bclary/WebDev/">Home</a>
+<a href="https://userweb.jlab.org/~bclary/WebDev/testphp/index.php">Home</a>
 <a href="https://userweb.jlab.org/~bclary/WebDev/testphp/dvcseg1/dvcseg1.php">DVCS Studies (EG1)</a>
 <a href="https://userweb.jlab.org/~bclary/WebDev/richdev/">RICH Developement</a>
 <a href="https://userweb.jlab.org/~bclary/WebDev/thesiswork/">Thesis Work</a>
