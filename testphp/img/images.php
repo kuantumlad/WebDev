cd1d00bb29e81213cb24fa8ac5f4c69cad69cb33
kuantumlad <brandon.clary@uconn.edu>
2016-07-04 00:24:19 -0400

    test if loops work?

--- testphp/img/images.php
+++ testphp/img/images.php
@@ -1,9 +1,9 @@
 <?php
 //$directory = "img/";
 //$images = glob("" . $directory . "*.jpg");
-foreach(glob('/userweb/bclary/public_html/WebDev/testphp/img/*.jpg' as $image){ $imgs[] = "$image"; }
+foreach(glob('/userweb/bclary/public_html/WebDev/testphp/img/*.jpg' as $image){ $imgs[] = "$image"; echo '<p> HELLO </p>';}
 foreach($imgs as $img){
-echo "<img src='$img' />";
+echo '<img src='$img' />';
 }
 ?>
 
