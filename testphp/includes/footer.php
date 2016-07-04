cf195b57c6f0020b896e60e5def37f1bca69a900
kuantumlad <brandon.clary@uconn.edu>
2016-07-03 20:56:33 -0400

    add footer

---
+++ testphp/includes/footer.php
@@ -0,0 +1,10 @@
+<?php
+echo '<div id="footer">
+    <ol style="list-style-type:none">
+      <li>Advisor: Kyungseon Joo - email: kjoo@phys.uconn.edu</li>
+      <li>Graduate Student: Brandon A. Clary - email:brandon.clary@uconn.edu</li>
+      <li>Copyright © Brandon Clary</li>
+      <li><!-- hhmts start -->Last modified: Sun Jul  3 01:57:32 EDT 2016 <!-- hhmts end --></li>
+    </ol>
+</div>'
+?>
--- testphp/index.php
+++ testphp/index.php
@@ -41,17 +41,7 @@
 
   <h1 id="ResearchUpdates">Updates:</h1>
   
-
-
-  <div id="footer">
-    <ol style="list-style-type:none">
-      <li>Advisor: Kyungseon Joo - email: kjoo@phys.uconn.edu</li>
-      <li>Graduate Student: Brandon A. Clary - email:brandon.clary@uconn.edu</li>
-      <li>Copyright © Brandon Clary</li>
-      <li><!-- hhmts start -->Last modified: Sun Jul  3 01:57:32 EDT 2016 <!-- hhmts end --></li>
-    </ol>
-</div>
-
+<?php include 'includes/footer.php';?>
 
 </body>
 
