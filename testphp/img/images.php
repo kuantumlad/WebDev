46c8a4cce9a9df9cad99a85b060171d3f20def21
kuantumlad <brandon.clary@uconn.edu>
2016-07-04 00:22:11 -0400

    1

--- testphp/img/images.php
+++ testphp/img/images.php
@@ -1,12 +1,9 @@
 <?php
-    //$directory = "img/";
-
-    //$images = glob("" . $directory . "*.jpg");
-
-    foreach(glob('/userweb/bclary/public_html/WebDev/testphp/img/*.jpg' as $image){ $imgs[] = "$image"; }
-            
-    foreach($imgs as $img){
-    echo "<img src='$img' />";
-    }
+//$directory = "img/";
+//$images = glob("" . $directory . "*.jpg");
+foreach(glob('/userweb/bclary/public_html/WebDev/testphp/img/*.jpg' as $image){ $imgs[] = "$image"; }
+foreach($imgs as $img){
+echo "<img src='$img' />";
+}
 ?>
 
