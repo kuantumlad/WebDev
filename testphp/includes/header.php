6b6aff946eae634429214116677da67f9e78994d
kuantumlad <brandon.clary@uconn.edu>
2016-07-14 16:02:20 -0400

    header update

--- testphp/includes/header.php
+++ testphp/includes/header.php
@@ -2,8 +2,8 @@
 echo '<div id="navigationContainer">    
 <a href="https://userweb.jlab.org/~bclary/WebDev/testphp/index.php">Home</a>
 <a href="https://userweb.jlab.org/~bclary/WebDev/testphp/dvcseg1/dvcseg1.php">DVCS Studies (EG1)</a>
-<a href="https://userweb.jlab.org/~bclary/WebDev/richdev/">RICH Developement</a>
-<a href="https://userweb.jlab.org/~bclary/WebDev/thesiswork/">Thesis Work</a>
+<a href="https://userweb.jlab.org/~bclary/WebDev/testphp/richdev/">RICH Developement</a>
+<a href="https://userweb.jlab.org/~bclary/WebDev/testphp/thesiswork/">Thesis Work</a>
 <a href="https://userweb.jlab.org/~bclary/WebDev/codedb/">Code Database</a>
 <a href="https://userweb.jlab.org/~bclary/WebDev/clas12wiki/">CLAS(12) Wiki</a>
 </div>';
