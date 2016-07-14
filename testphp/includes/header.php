d2c082d15bdc9f0c1276abab0cbc90961a0108fc
kuantumlad <brandon.clary@uconn.edu>
2016-07-14 16:03:31 -0400

    ud

--- testphp/includes/header.php
+++ testphp/includes/header.php
@@ -2,8 +2,8 @@
 echo '<div id="navigationContainer">    
 <a href="https://userweb.jlab.org/~bclary/WebDev/testphp/index.php">Home</a>
 <a href="https://userweb.jlab.org/~bclary/WebDev/testphp/dvcseg1/dvcseg1.php">DVCS Studies (EG1)</a>
-<a href="https://userweb.jlab.org/~bclary/WebDev/testphp/richdev/">RICH Developement</a>
-<a href="https://userweb.jlab.org/~bclary/WebDev/testphp/thesiswork/">Thesis Work</a>
+<a href="https://userweb.jlab.org/~bclary/WebDev/testphp/richdev/richdev.php">RICH Developement</a>
+<a href="https://userweb.jlab.org/~bclary/WebDev/testphp/thesiswork/thesiswork.php">Thesis Work</a>
 <a href="https://userweb.jlab.org/~bclary/WebDev/codedb/">Code Database</a>
 <a href="https://userweb.jlab.org/~bclary/WebDev/clas12wiki/">CLAS(12) Wiki</a>
 </div>';
