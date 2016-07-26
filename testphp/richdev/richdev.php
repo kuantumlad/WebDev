e5a830e4b9e88ff22be2b199843ce1b3bcfac5da
kuantumlad <brandon.clary@uconn.edu>
2016-07-25 21:35:21 -0400

    rich trigger notes

--- testphp/richdev/richdev.php
+++ testphp/richdev/richdev.php
@@ -35,6 +35,41 @@ current trigger analysis.
 ?>
 </table>
 
+</h1> Analyzing MC .dat.evio Files </h1>
+</p> To begin understanding CLAS12 analysis of evio files visit the
+site SITE NAME. For the working version of gemc the evio files do not
+contain simulated raw DAQ output. Thus reading the evio at this state
+will not reproduce the branches that correspond to the crate numbers
+for each event (running the code will leave you with a long list of
+event numbers).
+
+To start we covers the basic concepts of the CLAS/IO library. The
+CLAS/IO library is a java packakege meant to handle input and output
+of common CLAS data formats such as BOS?FPACK, EVIO (new for CLAS12),
+and iG5 which is a common data mining format. One can store
+information for each event in a specific detector by using a data
+format known as a "bank." The bank is definitions are stored in
+extensive markup language (XML) files. Each file can contain multiple
+bank definitions for a single detector. This is useful to seperate
+banks that handle raw generated and reconstructed events. One may
+divide each bank into sections which are identified with an XML
+attribute called "tag." Within the section varaibles are identified
+with a num attribute starting with 0 in the section tag. These banks
+can be created to handle user specific requirements. 
+
+INSERT EXAMPLE CODE
+
+Set up the environment by typing source
+/group/clas12/environment_java_8.csh. This will give access to evio
+files via the command line.
+
+To see the banks associated with the evio output files it is enough to
+type: evio_dump -i dvcsgen150.dat.evio in the command line. The banks
+are initialized with two columns nrows and ncols. Each bank varies
+between events because it depends on the whether or not a
+detector recorded a specific event. (is this true). 
+</p>
+
 </body>
 
 <?php include '../includes/footer.php';?>
