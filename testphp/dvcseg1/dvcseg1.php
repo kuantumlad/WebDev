c45caf552004b1edf809c1f5c9d790a01e2168dd
Brandon Clary <kuantumlad1@gmail.com>
2016-08-15 21:24:13 -0400

    img display for dvcseg1 page

--- testphp/dvcseg1/dvcseg1.php
+++ testphp/dvcseg1/dvcseg1.php
@@ -14,7 +14,28 @@
 
 <body>
 
+  <?php 
 
+     $dir = '/home/brandon/Documents/WebDev/testphp/img';
+     $file_display = array( 'jpeg', 'jpg', 'png', 'gif' );
+
+     if( file_exists($dir) == false ){
+     echo 'Directory not found';
+     } else {
+     $dir_contents = scandir($dir);
+
+     foreach ($dir_contents as $file){
+     $file_type = strtolower(end(explode('.',$file)));
+
+     if( $file !== '.' && $file !== '..' && in_array($file_type, $file_display) == true) {
+
+     echo '<img src="'. $dir. '/'. $file . '"alt="'.$file.'"/>';
+     }
+     }
+     }
+
+
+     ?>
 
 </body>
 
