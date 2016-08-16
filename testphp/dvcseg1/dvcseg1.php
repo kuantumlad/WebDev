6aa37bdab3b735e6e49a8bbdd073ee84ec52068f
Brandon Clary <kuantumlad1@gmail.com>
2016-08-15 21:53:19 -0400

    dir change

--- testphp/dvcseg1/dvcseg1.php
+++ testphp/dvcseg1/dvcseg1.php
@@ -20,7 +20,7 @@
      $file_display = array( 'jpeg', 'jpg', 'png', 'gif' );
 
      if( file_exists($dir) == false ){
-     echo 'Directory not found';
+     echo 'Directory''. $dir ' 'not found';
      } else {
      $dir_contents = scandir($dir);
 
