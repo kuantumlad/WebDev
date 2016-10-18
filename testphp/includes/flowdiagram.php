871cfef428716db15ae99f081588e55b02ddde09
kuantumlad <brandon.clary@uconn.edu>
2016-10-18 16:15:27 +0200

    WebDev commit in Paris

---
+++ testphp/includes/flowdiagram.php
@@ -0,0 +1,33 @@
+<?php
+
+echo '<!--
+We will create a family tree using just CSS(3)
+The markup will be simple nested lists
+-->
+<div class="tree">
+	<ul>
+		<li>
+			<a href="#">h22 ntuple data</a>
+			<ul>
+				<li>
+					<a href="#">Stage 1 Cuts</a>
+					<ul>
+						<li>
+							<a href="#">Stage 2 Cuts</a>
+							<ul>
+								<li>
+									<a href="#">Elastic Histograms</a>
+								</li>
+								<li>
+									<a href="#">Inelastic Histograms</a>
+								</li>
+								
+							</ul>
+						</li>
+					</ul>
+				</li>
+			</ul>
+		</li>
+	</ul>
+</div>'
+  ;?>
--- testphp/thesiswork/thesiswork.php
+++ testphp/thesiswork/thesiswork.php
@@ -14,7 +14,52 @@
 
 <body>
 
-Thesis Work
+<h1 id = "CS" >Current Status </h1>
+
+Developement and implementation of exclusivity cuts as
+prescribed by the dvcs_note_v30 document.
+
+<h1 id = "CS" >Completed Items </h1>
+
+<?php include '../includes/flowdiagram.php';?>
+
+Stage 1 Cuts include :
+<ul id="Cuts">
+  <li>Electron</li>
+  <ul>
+    <li>Negative Charge</li>
+    <li>Momentum Cut </li>
+    <li>Energy deposited in the Inner Calorimeter</li>
+    <li>Energy deposited in the Outer Calorimeter</li>
+    <li>Cherenkov Signal Time Cut</li>
+    <li>Event Vertez Cut</li>
+    <li>UVW Place Cuts</li>
+    <li>IC Shadow Cut</li>
+  </ul>
+  <li>Proton</li>
+  <ul>
+    <li>Positive Charge</li>
+    <li>Momentum Cut</li>
+    <li>Delta Beta Cut</li>
+    <li>Energy deposited in the Inner Calorimeter</li>
+    <li>Energy deposited in the Outer Calorimeter</li>
+    <li>Event Vertez Cut</li>
+  </ul>
+  <li>Photon</li>
+  <ul>
+    <li>Neutral Charge</li>
+    <li>IC Fiducial Cut</li>
+    <li>Moeller Electron Cut</li>
+    <li>Beta Cut</li>
+    <li>EC Energy Deposited Cut</li>
+    <li> UVW Place Cut</li>
+  </ul>
+</ul>
+  
+  
+    
+    
+
 
 </body>
 
